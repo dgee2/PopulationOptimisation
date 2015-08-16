88100264f9b3a4d5bce5526401b9c89ef22431d6
Daniel Gee <dan@dan-gee.co.uk>
2015-08-16 22:17:32 +0100

    Add Optimiser library

---
+++ src/Optimiser/OptimiserParticle.cs
@@ -0,0 +1,63 @@
+﻿using com.gee.ParticleSwarmOptimisation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Optimiser
+{
+	public class OptimiserParticle : Particle<OptimiserParticle>, IComparable<OptimiserParticle>
+	{
+		public static Random RandomGenerator;
+		static double slowdownFactor = .9;
+		static double accelerationFactor = .5;
+		public static Func<IList<double>, double> Function;
+		public static int Variables;
+
+		public OptimiserParticle()
+		{
+			Position = new List<double>(Variables);
+			Speed = new List<double>(Variables);
+			for (int i = 0; i < Variables; i++)
+			{
+				Position.Add(RandomGenerator.NextDouble() * 2.0 - 1.0);
+				Speed.Add(RandomGenerator.NextDouble() * 2.0 - 1.0);
+			}
+		}
+
+		public override IEnumerable<OptimiserParticle> Iterate(ISwarm<OptimiserParticle> swarm)
+		{
+			OptimiserParticle particle = new OptimiserParticle();
+			particle.Position = Position.ToList();
+			particle.Speed = Speed.ToList();
+			for (int i = Variables - 1; i >= 0; i--)
+			{
+				particle.Speed[i] *= slowdownFactor;
+			}
+			particle.AccelerateTowards(swarm.Particles.Max());
+			particle.IteratePosition();
+			yield return particle;
+		}
+
+		private void AccelerateTowards(OptimiserParticle particle)
+		{
+			IList<double> distance = GetDistance(particle).ToList();
+			for (int i = Speed.Count - 1; i >= 0; i--)
+			{
+				Speed[i] += distance[i] * accelerationFactor;
+			}
+		}
+
+		public int CompareTo(OptimiserParticle other)
+		{
+			return Quality.CompareTo(other.Quality);
+		}
+
+		public double Quality
+		{
+			get
+			{
+				return Function(Position);
+			}
+		}
+	}
+}
---
+++ src/Optimiser/Runner.cs
@@ -0,0 +1,31 @@
+﻿using com.gee.ParticleSwarmOptimisation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Optimiser
+{
+	public class Runner
+	{
+		public static void Run(Func<IList<double>, double> function, int variables, int iterations, int populationSize, int swarmSize)
+		{
+			OptimiserParticle.Function = function;
+			OptimiserParticle.RandomGenerator = new Random();
+			OptimiserParticle.Variables = variables;
+			IList<Swarm<OptimiserParticle>> swarms = new List<Swarm<OptimiserParticle>>(populationSize);
+			for (int i = 0; i < populationSize; i++)
+			{
+				swarms.Add(new Swarm<OptimiserParticle>(swarmSize));
+			}
+			uint iteration = 0;
+			Console.Write(string.Join(",", swarms.Select(swarm => swarm.Particles.Max(x => x.Quality)).ToArray()));
+			for (; iteration < iterations; iteration++)
+			{
+				Parallel.ForEach(swarms, swarm => swarm.IterateSwarm());
+				Console.WriteLine();
+				Console.Write(string.Join(",", swarms.Select(swarm => swarm.Particles.Max(x => x.Quality)).ToArray()));
+			}
+		}
+	}
+}
