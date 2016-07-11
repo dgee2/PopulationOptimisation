32580cc8c8066dc8a36f503577722304a820ba5f
Daniel Gee <dan@dan-gee.co.uk>
2016-07-11 23:41:47 +0100

    Convert to use standard c# projects and the CommandLineParser library

---
+++ src/ParticleSwarmOptimisationConsole/Options.cs
@@ -0,0 +1,50 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSwarmOptimisationConsole
+{
+	// Define a class to receive parsed values
+	class Options
+	{
+		[Option("variables",
+			HelpText = "The number of variables",
+			DefaultValue = 1)]
+		public int Variables { get; set; }
+
+		[Option("iterations",
+			HelpText = "The number of iterations to run until",
+			DefaultValue = 100)]
+		public int Iterations { get; set; }
+
+		[Option("populationSize",
+			HelpText = "The size of the population to use",
+			DefaultValue = 50)]
+		public int PopulationSize { get; set; }
+
+		[Option("swarmSize",
+			HelpText = "The size of the swarm to use",
+			DefaultValue = 100)]
+		public int SwarmSize { get; set; }
+
+		[Option('a',
+			"algorithm",
+			HelpText = "The algorithm to use",
+			Required = true)]
+		public string Algorithm { get; set; }
+
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this,
+			  (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
--- src/ParticleSwarmOptimisationConsole/Program.cs
+++ src/ParticleSwarmOptimisationConsole/Program.cs
@@ -1,74 +1,43 @@
 ﻿using Optimiser;
 using System;
-using Microsoft.Framework.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ParticleSwarmOptimisation
+namespace ParticleSwarmOptimisationConsole
 {
 	public class Program
 	{
-		public void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			var config = new ConfigurationBuilder()
-							.AddCommandLine(args).Build();
-
-			string algorithm;
-			string strVariables;
-			string strIterations;
-			string strPopulationSize;
-			string strSwarmSize;
-
-			if (!config.TryGet("variables", out strVariables))
+			var config = new Options();
+			if (CommandLine.Parser.Default.ParseArguments(args, config))
 			{
-				strVariables = "1";
-			}
-			int variables = int.Parse(strVariables);
-			if (!config.TryGet("iterations", out strIterations))
-			{
-				strIterations = "100";
-			}
-			int iterations = int.Parse(strIterations);
-			if (!config.TryGet("populationSize", out strPopulationSize))
-			{
-				strPopulationSize = "50";
-			}
-			int populationSize = int.Parse(strPopulationSize);
-			if (!config.TryGet("swarmSize", out strSwarmSize))
-			{
-				strSwarmSize = "100";
-			}
-			int swarmSize = int.Parse(strSwarmSize);
-
-			if (!config.TryGet("algorithm", out algorithm))
-			{
-				throw new Exception("Algorithm not set");
-			}
-			Func<IList<double>, double> function;
-			switch (algorithm)
-			{
-				case "Test":
-					function = (x) =>
-					{
-						return x[0] * (1 - x[0]) * Math.Exp(x[0]);
-					};
-					break;
-				case "StyblinskiTang":
-					function = (x) =>
-					{
-						x = x.Select(y=> Math.Max(Math.Min(y*5, 5), -5)).ToList();
-						double max = -x.Sum(y => Math.Pow(y, 4) - 16 * Math.Pow(y, 2) + 5 * y) / 2;
-						if (max > 78.4)
+				Func<IList<double>, double> function;
+				switch (config.Algorithm)
+				{
+					case "Test":
+						function = (x) =>
 						{
-							int a = 1;
-						}
-						return max;
-					};
-					break;
-				default:
-					throw new NotImplementedException("Algorithm has not been implemented yet");
+							return x[0] * (1 - x[0]) * Math.Exp(x[0]);
+						};
+						break;
+					case "StyblinskiTang":
+						function = (x) =>
+						{
+							x = x.Select(y => Math.Max(Math.Min(y * 5, 5), -5)).ToList();
+							double max = -x.Sum(y => Math.Pow(y, 4) - 16 * Math.Pow(y, 2) + 5 * y) / 2;
+							if (max > 78.4)
+							{
+								int a = 1;
+							}
+							return max;
+						};
+						break;
+					default:
+						throw new NotImplementedException("Algorithm has not been implemented yet");
+				}
+				Runner.Run(function, config.Variables, config.Iterations, config.PopulationSize, config.SwarmSize);
 			}
-			Runner.Run(function, variables, iterations, populationSize, swarmSize);
 		}
 	}
 }
