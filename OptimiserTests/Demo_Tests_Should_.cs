c9d7f38dc5ed6ef1f68a1274edc3c5a127aa7460
Daniel Gee <dan@dan-gee.co.uk>
2016-07-13 21:28:47 +0100

    Add OptimiserTests project with a pair of demo tests

---
+++ OptimiserTests/Demo_Tests_Should_.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace OptimiserTests
+{
+	public class Demo_Tests_Should_
+	{
+		[Test]
+		public void Pass() => Assert.Pass();
+
+		[Test]
+		public void Fail() => Assert.Fail();
+	}
+}
